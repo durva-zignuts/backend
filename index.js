de133518aeeb55511ac45bafefc90831f057ade1
= <=>
2023-10-26 12:58:08 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+mongoose.set("strictQuery", false);
+mongoose.connect(
+  "mongodb+srv://ecommerce:Ecommerce@cluster0.z3qj8nv.mongodb.net/",
+  { useNewUrlParser: true }
+);
+
+mongoose.Promise = global.Promise;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  return res.end("Hello World");
+});
+
+module.exports = app;
